d8d03719f64999713d26cdd232c5208d37ec190e
epitchi <thienvanlea1@gmail.com>
2022-12-27 20:21:58 +0700

    Update submodule

--- lib/main.dart
+++ lib/main.dart
@@ -36,28 +36,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int? _counter = 0;
   late MethodChannel _channel;
 
   @override
   void initState() {
     super.initState();
     _channel = const MethodChannel('multiple-flutters');
-    _channel.setMethodCallHandler((call) async {
-      if (call.method == "setCount") {
-        // A notification that the host platform's data model has been updated.
-        setState(() {
-          _counter = call.arguments as int?;
-        });
-      } else {
-        throw Exception('not implemented ${call.method}');
-      }
-    });
   }
 
-  void _incrementCounter() {
-    // Mutations to the data model are forwarded to the host platform.
-    _channel.invokeMethod<void>("incrementCount", _counter);
+  void startGame(String token) {
+    _channel.invokeMethod<void>("startGame", token);
   }
 
   @override
@@ -70,33 +58,9 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
             TextButton(
-              onPressed: _incrementCounter,
-              child: const Text('Add'),
-            ),
-            TextButton(
-              onPressed: () {
-                _channel.invokeMethod<void>("next", _counter);
-              },
-              child: const Text('Next'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                // Use the url_launcher plugin to open the Flutter docs in
-                // a browser.
-                final url = Uri.parse('https://flutter.dev/docs');
-                if (await launcher.canLaunchUrl(url)) {
-                  await launcher.launchUrl(url);
-                }
-              },
-              child: const Text('Open Flutter Docs'),
+              onPressed: () => startGame("abc"),
+              child: const Text('Start Game'),
             ),
           ],
         ),
