c922f38028b5fb5d813d584e04968cf2a1133774
epitchi <thienvanlea1@gmail.com>
2022-12-29 12:47:02 +0700

    add alert form fill token

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart' as launcher;
+import 'package:clipboard/clipboard.dart';
 
 void main() => runApp(const MyApp(color: Colors.blue));
 
@@ -37,6 +38,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late MethodChannel _channel;
+  TextEditingController tokenCtrler = TextEditingController();
 
   @override
   void initState() {
@@ -59,12 +61,53 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextButton(
-              onPressed: () => startGame("abc"),
+              onPressed: () => displayTextInputDialog(context),
               child: const Text('Start Game'),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void displayTextInputDialog(BuildContext context) async {
+    tokenCtrler.text = await FlutterClipboard.paste();
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Please fill your token!'),
+            content: TextField(
+              controller: tokenCtrler,
+              decoration: InputDecoration(
+                  hintText: "Your token",
+                  suffixIcon: InkWell(
+                    onTap: () {
+                      tokenCtrler.clear();
+                    },
+                    child: const Icon(Icons.close_rounded),
+                  )),
+            ),
+            actions: [
+              MaterialButton(
+                color: Colors.red,
+                textColor: Colors.white,
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              MaterialButton(
+                color: Colors.green,
+                textColor: Colors.white,
+                child: const Text('OK'),
+                onPressed: () {
+                  startGame(tokenCtrler.text);
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        });
   }
 }
