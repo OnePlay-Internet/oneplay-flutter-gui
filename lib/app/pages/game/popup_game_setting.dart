2894149a9da38276baf6aee787e9ce05ad582e9e
epitchi <thienvanlea1@gmail.com>
2023-02-15 13:00:13 +0700

    rollback commit

--- lib/app/pages/game/popup_game_setting.dart
+++ lib/app/pages/game/popup_game_setting.dart
@@ -20,163 +20,161 @@ Widget gameSettingPopup(
       backgroundColor: mainColor,
       contentPadding: EdgeInsets.zero,
       insetPadding: const EdgeInsets.symmetric(horizontal: 20),
-      content: Expanded(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height * 0.6,
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 30, horizontal: 8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Select your game settings',
-                      style: TextStyle(
-                        fontFamily: mainFontFamily,
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                        letterSpacing: 0.02,
-                      ),
+      content: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.8,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Select your game settings',
+                    style: TextStyle(
+                      fontFamily: mainFontFamily,
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 0.02,
                     ),
-                    FocusZoom(
-                      builder: (focus) {
-                        return InkWell(
-                          focusNode: focus,
-                          onTap: () => Navigator.pop(context),
-                          child: SvgPicture.asset(
-                            crossIcon,
-                            height: 20,
-                            width: 20,
-                          ),
-                        );
-                      },
-                    )
-                  ],
-                ),
+                  ),
+                  FocusZoom(
+                    builder: (focus) {
+                      return InkWell(
+                        focusNode: focus,
+                        onTap: () => Navigator.pop(context),
+                        child: SvgPicture.asset(
+                          crossIcon,
+                          height: 20,
+                          width: 20,
+                        ),
+                      );
+                    },
+                  )
+                ],
               ),
-              commonDividerWidget(),
-              Container(
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                  height: 38,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(6),
-                      color: const Color(0xffE31C34)),
-                  child: Center(
-                      child: Text('You need to own this game in steam.',
-                          style: tinyStyle.copyWith(fontSize: 15)))),
-              Container(
-                margin: const EdgeInsets.only(top: 10, bottom: 10, left: 20),
-                alignment: Alignment.topLeft,
-                child: Text(
-                  'Resolution',
-                  style: tinyStyle.copyWith(color: textSecondaryColor),
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                height: 52,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(color: basicLineColor, width: 2)),
+            ),
+            commonDividerWidget(),
+            Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                height: 38,
                 width: MediaQuery.of(context).size.width,
-                child: dropdownMenu(
-                    PlayConstants.MOBILE_RESOLUTIONS, gameSetting.resolution),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
+                    color: const Color(0xffE31C34)),
+                child: Center(
+                    child: Text('You need to own this game in steam.',
+                        style: tinyStyle.copyWith(fontSize: 15)))),
+            Container(
+              margin: const EdgeInsets.only(top: 10, bottom: 10, left: 20),
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Resolution',
+                style: tinyStyle.copyWith(color: textSecondaryColor),
               ),
-              const SizedBox(height: 30),
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                        child: Container(
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              height: 52,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: basicLineColor, width: 2)),
+              width: MediaQuery.of(context).size.width,
+              child: dropdownMenu(
+                  PlayConstants.MOBILE_RESOLUTIONS, gameSetting.resolution),
+            ),
+            const SizedBox(height: 30),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                      child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    height: 52,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: basicLineColor, width: 2)),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Vsync',
+                            style:
+                                tinyStyle.copyWith(color: textPrimaryColor),
+                          ),
+                          StatefulBuilder(
+                            builder: (_, setState) => SizedBox(
+                              height: 20,
+                              width: 34,
+                              child: Transform.scale(
+                                scale: 0.6,
+                                child: CupertinoSwitch(
+                                  value: gameSetting.is_vsync_enabled!,
+                                  thumbColor: textPrimaryColor,
+                                  activeColor: Colors.purple,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      gameSetting.is_vsync_enabled = value;
+                                    });
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+                        ]),
+                  )),
+                  const SizedBox(width: 30),
+                  Expanded(
+                    child: Container(
                       padding: const EdgeInsets.symmetric(horizontal: 20),
                       height: 52,
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10),
-                          border: Border.all(color: basicLineColor, width: 2)),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Vsync',
-                              style:
-                                  tinyStyle.copyWith(color: textPrimaryColor),
-                            ),
-                            StatefulBuilder(
-                              builder: (_, setState) => SizedBox(
-                                height: 20,
-                                width: 34,
-                                child: Transform.scale(
-                                  scale: 0.6,
-                                  child: CupertinoSwitch(
-                                    value: gameSetting.is_vsync_enabled!,
-                                    thumbColor: textPrimaryColor,
-                                    activeColor: Colors.purple,
-                                    onChanged: (value) {
-                                      setState(() {
-                                        gameSetting.is_vsync_enabled = value;
-                                      });
-                                    },
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ]),
-                    )),
-                    const SizedBox(width: 30),
-                    Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        height: 52,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            border:
-                                Border.all(color: basicLineColor, width: 2)),
-                        child: dropdownMenu(PlayConstants.FPS, gameSetting.fps),
-                      ),
+                          border:
+                              Border.all(color: basicLineColor, width: 2)),
+                      child: dropdownMenu(PlayConstants.FPS, gameSetting.fps),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 30),
-              FocusZoom(builder: (focus) {
-                return InkWell(
-                  focusNode: focus,
-                  onTap: () => advancedSettingPopup(
-                    context,
-                    gameSetting,
-                    launchGame,
-                  ),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SvgPicture.asset(advancedSettingIcon,
-                              height: 20, color: textPrimaryColor),
-                          const SizedBox(width: 14),
-                          Text(
-                            'Advanced game options',
-                            style:
-                                tinyStyle.copyWith(color: textSecondaryColor),
-                          )
-                        ]),
-                  ),
-                );
-              }),
-              btnLaunchGame(context, launchGame)
-            ],
-          ),
+            ),
+            const SizedBox(height: 30),
+            FocusZoom(builder: (focus) {
+              return InkWell(
+                focusNode: focus,
+                onTap: () => advancedSettingPopup(
+                  context,
+                  gameSetting,
+                  launchGame,
+                ),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(advancedSettingIcon,
+                            height: 20, color: textPrimaryColor),
+                        const SizedBox(width: 14),
+                        Text(
+                          'Advanced game options',
+                          style:
+                              tinyStyle.copyWith(color: textSecondaryColor),
+                        )
+                      ]),
+                ),
+              );
+            }),
+            btnLaunchGame(context, launchGame)
+          ],
         ),
       ));
 }
@@ -302,7 +300,7 @@ Container selectionGameSetting(BuildContext context, String title,
   return Container(
     height: 88,
     width: MediaQuery.of(context).size.width,
-    margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+    margin: const EdgeInsets.symmetric(horizontal: 17, vertical: 15),
     child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -312,39 +310,52 @@ Container selectionGameSetting(BuildContext context, String title,
             style: tinyStyle.copyWith(color: textSecondaryColor),
           ),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: audioType
-                .map((e) => FocusZoom(builder: (focus) {
-                      return InkWell(
-                        focusNode: focus,
-                        onTap: () => onTap(e),
-                        child: Container(
-                          height: 52,
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 12, vertical: 13),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              border: Border.all(
-                                  width: 2,
-                                  color: audioType.indexOf(e) == audioTypeValue
-                                      ? purpleColor1
-                                      : basicLineColor)),
-                          child: GradientText(
-                            e,
-                            style: tinyStyle,
-                            gradientType: GradientType.linear,
-                            gradientDirection: GradientDirection.ltr,
-                            colors: audioType.indexOf(e) == audioTypeValue
-                                ? const [purpleColor2, purpleColor1]
-                                : [textPrimaryColor, textPrimaryColor],
-                          ),
-                        ),
-                      );
-                    }))
-                .toList(),
-          )
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children:
+                  addElementIntoListWidget(audioType, onTap, audioTypeValue))
         ]),
   );
+}
+
+List<Widget> addElementIntoListWidget(
+    List<String> data, Function(String e) onTap, int value) {
+  List<Widget> widgets = [];
+  data.forEach((element) {
+    if (data.indexOf(element) > 0 && data.indexOf(element) < data.length) {
+      widgets.add(const SizedBox(
+        width: 30,
+      ));
+    }
+    widgets.add(Flexible(
+      fit: FlexFit.loose,
+      child: InkWell(
+        onTap: () => onTap(element),
+        child: Container(
+          height: 52,
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 13),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                  width: 2,
+                  color: data.indexOf(element) == value
+                      ? purpleColor1
+                      : basicLineColor)),
+          child: Center(
+            child: GradientText(
+              element,
+              style: tinyStyle,
+              gradientType: GradientType.linear,
+              gradientDirection: GradientDirection.ltr,
+              colors: data.indexOf(element) == value
+                  ? const [purpleColor2, purpleColor1]
+                  : [textPrimaryColor, textPrimaryColor],
+            ),
+          ),
+        ),
+      ),
+    ));
+  });
+  return widgets;
 }
 
 Container switchGameSetting({
