ae87c3388ea183e4c967134935288c0ecff7f8bb
epitchi <thienvanlea1@gmail.com>
2023-02-28 12:48:34 +0700

    resolve conflict

--- lib/app/widgets/bottom_nav/bottom_nav.dart
+++ lib/app/widgets/bottom_nav/bottom_nav.dart
@@ -116,6 +116,17 @@ class _BottomNavState extends State<BottomNav> {
   }
 
   void _onTap(int value) {
+    if (previousIndex == value) {
+      previousIndex = selectedIndex;
+    }
+    
+    if (value == selectedIndex) {
+      // unable Tap on selected in setting screen
+      if (value == 4) {
+        return;
+      }
+    }
+    
     setState(() {
       selectedIndex = value;
       print('***** $selectedIndex *****');
