68c30e169d507e193ab2613312b6a5b78748d3b6
Sergey Strelchenko <serguncheouss@gmail.com>
2023-03-07 16:13:43 +0530

    Added constants for controller device events.

---
+++ lib/app/common/utils/controller_handler.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: constant_identifier_names
+
+class ControllerHandler {
+  // Events
+  static const ON_CONTROLLER_CONNECTED_EVENT = "on_controller_connected_event";
+  static const ON_CONTROLLER_DISCONNECTED_EVENT = "on_controller_disconnected_event";
+  static const ON_CONTROLLER_RECEIVED_INPUT_EVENT = "on_controller_received_input_event";
+  // Params
+  static const DEVICE_ID = "device_id";
+  static const VENDOR_ID = "vendor_id";
+  static const PRODUCT_ID = "product_id";
+  static const CONTROLLER_NUMBER = "controller_number";
+  static const GAMEPAD_MASK = "gamepad_mask";
+  static const BUTTON_FLAGS = "button_flags";
+  static const LEFT_TRIGGER = "left_trigger";
+  static const RIGHT_TRIGGER = "right_trigger";
+  static const LEFT_STICK_X = "left_stick_x";
+  static const LEFT_STICK_Y = "left_stick_y";
+  static const RIGHT_STICK_X = "right_stick_x";
+  static const RIGHT_STICK_Y = "right_stick_y";
+}
